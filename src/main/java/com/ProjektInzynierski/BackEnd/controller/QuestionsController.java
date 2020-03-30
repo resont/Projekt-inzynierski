35501dbb1a715549cbf6c128c4ff8981236902ab
resont <wojtasfor@gmail.com>
2020-03-30 20:37:09 +0200

    add questions files

---
+++ src/main/java/com/ProjektInzynierski/BackEnd/controller/QuestionsController.java
@@ -0,0 +1,62 @@
+package com.ProjektInzynierski.BackEnd.controller;
+
+
+import com.ProjektInzynierski.BackEnd.datamodel.Questions;
+import com.ProjektInzynierski.BackEnd.repository.QuestionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class QuestionsController {
+
+    @Autowired
+    private QuestionsRepository questionsRepository;
+
+
+
+    @GetMapping("/questions/all")
+    List<Questions> getAllQuestionsFromDatabase() {
+        return this.questionsRepository.findAll();
+    }
+
+    @GetMapping("/questions/search")
+    Questions search(@RequestParam(name = "question") String question) {
+        return this.questionsRepository.findValidQuestionByTopic(question);
+    }
+
+    @GetMapping("/questions/{id}")
+    Questions show(@PathVariable("id") String id) {
+        int q_Id = Integer.parseInt(id);
+        return this.questionsRepository.findById(q_Id);
+    }
+
+    @PostMapping("/questions")
+    Questions create(@RequestBody Map<String, String> body) {
+        Questions questions = new Questions();
+        questions.setQuestion(body.get("question"));
+        questions.setQ_s_id(Integer.parseInt(body.get("q_s_id")));
+        return this.questionsRepository.save(questions);
+    }
+
+    //TODO possible null value
+    @PutMapping("/questions/{id}")
+    Questions update(@PathVariable String id, @RequestBody Map<String, String> body) {
+        Questions questions = this.questionsRepository.findById(Integer.parseInt(id));
+        questions.setQuestion(body.get("question"));
+        questions.setQ_s_id(Integer.parseInt(body.get("q_s_id")));
+        return this.questionsRepository.save(questions);
+    }
+
+    @DeleteMapping("/questions/{id}")
+    boolean delete(@PathVariable String id) {
+        Questions questions = new Questions();
+        questions.setId(Integer.parseInt(id));
+        this.questionsRepository.delete(questions);
+        return true;
+    }
+}
---
+++ src/main/java/com/ProjektInzynierski/BackEnd/repository/QuestionsRepository.java
@@ -0,0 +1,21 @@
+package com.ProjektInzynierski.BackEnd.repository;
+
+import com.ProjektInzynierski.BackEnd.datamodel.Questions;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionsRepository extends JpaRepository<Questions, Integer>, JpaSpecificationExecutor<Questions> {
+
+
+    List<Questions> findByIdAndQuestion(int id, String question);
+
+    Questions findById(int id);
+
+    @Query("SELECT q FROM Questions q WHERE q.question=:question")
+    Questions findValidQuestionByTopic(String question);
+}

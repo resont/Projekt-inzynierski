565622fdbe1d4e16afe40d9165e9d4cb13b4a363
resont <wojtasfor@gmail.com>
2020-03-30 20:37:09 +0200

    add questions files

---
+++ src/main/java/com/ProjektInzynierski/BackEnd/datamodel/Questions.java
@@ -0,0 +1,24 @@
+package com.ProjektInzynierski.BackEnd.datamodel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Questions")
+@Getter
+@Setter
+public class Questions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "q_id", unique = true)
+    private int id;
+
+    @Column(name = "q_question")
+    private String question;
+
+    @Column(name = "q_s_id")
+    private int q_s_id;
+}

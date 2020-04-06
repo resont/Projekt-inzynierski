24a98d3dee74b4a428dcb9650e97d023fee3841d
resont <wojtasfor@gmail.com>
2020-04-06 23:36:50 +0200

    add initial web structure

--- src/main/java/com/ProjektInzynierski/BackEnd/enums/LoginMsg.java
+++ src/main/java/com/ProjektInzynierski/BackEnd/enums/LoginMsg.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum LoginMsg {
-    WRONG_EMAIL_OR_PASSWORD("Wrong Email adress or password. Please try again."),
+    WRONG_EMAIL_OR_PASSWORD("Wrong Email address or password. Please try again."),
     IS_STILL_VALID("Your token is still valid.");
 
     private final String errorMsg;
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,17 +1,37 @@
-var xhr = new XMLHttpRequest();
+function register() {
+    var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-            document.body.innerText = 'Response: ' + xhr.responseText;
-        } else {
-            document.body.innerText = 'Error: ' + xhr.status;
+    result(xhr);
+
+    var email = $("#email").val();
+    var password = $("#password").val();
+    var data = '{"email":"'+email+'","password":"'+password+'"}';
+    xhr.open('POST', 'http://localhost:8080/registration', true);
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    xhr.send(data);
+}
+
+function login() {
+    var xhr = new XMLHttpRequest();
+
+    result(xhr);
+
+    var email = $("#email").val();
+    var password = $("#password").val();
+    var data = '{"email":"'+email+'","password":"'+password+'"}';
+    xhr.open('POST', 'http://localhost:8080/login', true);
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    xhr.send(data);
+}
+
+function result(xhr){
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                document.body.innerText = 'Response: ' + xhr.responseText;
+            } else {
+                document.body.innerText = 'Error: ' + xhr.status;
+            }
         }
-    }
-};
-
-var data = '{"email":"xhr-test2","password":"test"}';
-
-xhr.open('POST', 'http://localhost:8080/registration', true);
-xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-xhr.send(data);
+    };
+}

6ce018fe3eba46966cb6b43b7fb272b6bc32e0f2
resont <wojtasfor@gmail.com>
2020-05-09 23:19:34 +0200

    Add survey results to admin panel

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     showUsers();
+    pickSurvey();
     showLogoutAndProfile();
 };
 
@@ -59,13 +60,13 @@ function showUsers() {
     xhr.send(null);
 }
 
-function getAllSurveys(i) {
+function getAllSurveys(i, action = "") {
     var body = "";
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
             var surveys = JSON.parse(xhr.responseText);
-            body += "<select id=\"sel" + i + "\" class=\"form-control\">";
+            body += "<select id=\"sel" + i + "\" class=\"form-control\" onchange=" + action + ">";
             body += "<option></option>";
             for (var s in surveys) {
                 var survey = surveys[s];
@@ -123,4 +124,81 @@ function redirectToAdminPanel() {
     window.setTimeout(function () {
         location.href = "admin.html";
     }, 1000);
+}
+
+function pickSurvey() {
+    var body = `<div class="main-panel border rounded p-4">
+                <label class="lead" for="selSurvey">Select survey</label>`;
+    body += getAllSurveys("Survey", "showResults(this)");
+    body += "</div>";
+    $("body").append(body);
+}
+
+var resultBody = "";
+
+function showResults(selectObj) {
+    resultBody = "";
+    var id = selectObj.value;
+    var exists = true;
+    if (!$('#results').length) {
+        resultBody += `<div id="results" class="main-panel border rounded p-4">`;
+        exists = false;
+    }
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            var obj = JSON.parse(xhr.responseText);
+            obj["questions"].forEach(showQuestions);
+            if (exists === false) {
+                resultBody += "</div>";
+                $("body").append(resultBody);
+            } else {
+                $("#results").html(resultBody);
+            }
+
+        }
+    };
+    xhr.open('GET', 'http://localhost:8080/survey/' + id, false);
+    xhr.send(null);
+}
+
+var answersTotal = 0;
+var type = 0;
+
+function showQuestions(item, index) {
+    answersTotal = 0;
+    type = 0;
+    resultBody += '<div class="lead">' + item["question"] + '</div>';
+    if (parseInt(item["type"]) !== 1) {
+        item["answers"].forEach(calcAnswersTotal);
+    } else {
+        type = 1;
+    }
+    item["answers"].forEach(showAnswers);
+}
+
+function showAnswers(item, index) {
+    var percent = Math.round(parseInt(item["count"]) / answersTotal * 100);
+    resultBody += "<div class='font-weight-normal ml-4 mb-2'>";
+
+    if (answersTotal === 0) {
+        if (type !== 1) {
+            resultBody += item["answer"];
+            resultBody += "&emsp; NO ANSWERS";
+        } else {
+            //resultBody += '<textarea type="text" class="form-control" rows="1.5" disabled>'+item["answer"]+'</textarea>';
+            resultBody += "<div class='border pl-2 pt-2 pr-2 rounded' style='background-color: #eaeaea;'><p>"+item["answer"]+"</p></div>";
+        }
+    } else {
+        resultBody += item["answer"];
+        resultBody += `<div class="progress">
+                            <div class="progress-bar bg-dark" role="progressbar" style="width: `+percent+`%;" aria-valuenow="`+percent+`" aria-valuemin="0" aria-valuemax="100">`+percent+`%</div>
+                       </div>`;
+    }
+    resultBody += "</div>";
+}
+
+function calcAnswersTotal(item, index) {
+    answersTotal += item["count"];
 }

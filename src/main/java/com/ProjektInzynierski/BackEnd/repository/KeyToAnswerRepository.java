48c5fa62e9420d64a6ec4bc204671789dd6659d4
resont <wojtasfor@gmail.com>
2020-05-16 23:33:03 +0200

    Add connection between keys and answers part 2

---
+++ src/main/java/com/ProjektInzynierski/BackEnd/repository/KeyToAnswerRepository.java
@@ -0,0 +1,18 @@
+package com.ProjektInzynierski.BackEnd.repository;
+
+import com.ProjektInzynierski.BackEnd.data.entity.AnswerToKeyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+public interface KeyToAnswerRepository extends JpaRepository<AnswerToKeyEntity, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE AnswerToKeyEntity a SET a.keyEntityId.id=:kId, a.answersId.id=:aId WHERE a.id=:id")
+    void addAnswerToKey(int id, int kId, int aId);
+}
